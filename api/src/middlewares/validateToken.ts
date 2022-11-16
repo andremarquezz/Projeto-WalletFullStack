641231002b2719fd3dd4be5aeba75d62fc3c4206
Jey <andre.marqquez@gmail.com>
2022-11-16 12:07:14 -0300

    feat [middleware]:  validar o  token
    Cria middleware para validar o token

---
+++ api/src/middlewares/validateToken.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request } from 'express';
+import * as jwt from 'jsonwebtoken';
+import ErrorUnauthorized from '../errors/ErrorUnauthorized';
+import UserModel from '../database/models/UserModel';
+import IResponseToken from '../interfaces/IResponseToken';
+import IInfoToken from '../interfaces/IInfoToken';
+
+const { JWT_SECRET } = process.env;
+
+const validateToken = async (req: Request, res: IResponseToken, next: NextFunction) => {
+  const { authorization: token } = req.headers;
+  if (!token) throw new ErrorUnauthorized('token not found');
+
+  try {
+    const { username, userId } = jwt.verify(token, JWT_SECRET as string) as IInfoToken;
+    const user = await UserModel.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new ErrorUnauthorized('User not found');
+    }
+    res.locals.user = { username, userId };
+    next();
+  } catch (error) {
+    throw new ErrorUnauthorized('Token must be a valid token');
+  }
+};
+
+export default validateToken;
