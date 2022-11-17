f45b9dbb05d7023fe37537315e6bf27258e90330
Jey <andre.marqquez@gmail.com>
2022-11-17 14:03:18 -0300

    feat [package]: biblioteca para documentação

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,15 +4,16 @@ import IError from './interfaces/IError';
 import userRoutes from './routes/userRoutes';
 import accountRoutes from './routes/accountRoutes';
 import transactionRoutes from './routes/transactionRoutes';
-
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 
-app.use('/health', (_req: Request, res: Response): void => {
-  res.status(200).json({ message: 'message' });
-});
-
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(userRoutes);
 app.use(accountRoutes);
 app.use(transactionRoutes);
