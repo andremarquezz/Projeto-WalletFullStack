f126dba1d40c113ab98c9135378a5a226474193b
Jey <andre.marqquez@gmail.com>
2022-11-18 03:41:55 -0300

    feat: exemplos de envs necessárias para a API

--- api/src/database/config/database.ts
+++ api/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE || 'ngcash_db',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dialect: 'postgres',
