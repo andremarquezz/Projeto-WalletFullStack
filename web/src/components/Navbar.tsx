7ddba73a8af24d8b0bcb29d76055263511c1a53b
Jey <andre.marqquez@gmail.com>
2022-11-21 17:39:40 -0300

    feat: componente navbar

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <Link to={'/account'}>Realizar Transação</Link>
+        <Link to={'/account/transactionMade'}>Transações Feitas</Link>
+        <Link to="/login" onClick={() => localStorage.removeItem('user')}>
+          Sair
+        </Link>
+      </nav>
+      ;
+    </div>
+  );
+}
