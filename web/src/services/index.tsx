89eb0718bfd7b2718e5f1d9bf78b3111eebed9c2
Jey <andre.marqquez@gmail.com>
2022-11-21 17:39:25 -0300

    refactor: adiciona variavel para request axios

--- web/src/services/index.tsx
+++ web/src/services/index.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const { REACT_APP_PORT_API } = process.env;
+
+
 const api = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: `http://localhost:${REACT_APP_PORT_API}`,
 });
 
 export default api;
