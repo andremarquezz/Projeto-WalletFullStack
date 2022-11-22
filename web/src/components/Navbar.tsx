8da58904e9d4d93a6bdf8dd3fde35858f68f2fb8
Jey <andre.marqquez@gmail.com>
2022-11-22 19:16:38 -0300

    style: Adiciona estilo em navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -2,15 +2,22 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="d-flex justify-content-around navbar navbar-expand-lg navbar-light bg-light">
       <nav>
-        <Link to={'/account'}>Realizar Transação</Link>
-        <Link to={'/account/record'}>Transações Feitas</Link>
-        <Link to="/login" onClick={() => localStorage.removeItem('user')}>
+        <Link to={'/account'} className="p-4 text-decoration-none m-5 text-dark ">
+          Realizar Transação
+        </Link>
+        <Link to={'/account/record'} className="p-4 text-decoration-none m-5 text-dark">
+          Todas as Transações
+        </Link>
+        <Link
+          to="/login"
+          onClick={() => localStorage.removeItem('user')}
+          className="p-4 text-decoration-none text-danger"
+        >
           Sair
         </Link>
       </nav>
-      ;
     </div>
   );
 }
